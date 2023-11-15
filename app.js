90ae1d6593e8f6ac4ed9cb96ea0b9a65e8c6d977
Glitch (messenger-platform-quick-start) <none>
2023-11-15 17:21:21 +0000

    🏃💏 Checkpoint ./app.js:43257516/14894

--- app.js
+++ app.js
@@ -1,59 +1,118 @@
-const request = require('request');
+/*
+ * Starter Project for WhatsApp Echo Bot Tutorial
+ *
+ * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
+ *
+ */
 
+"use strict";
+
+// Access token for your app
+// (copy token from DevX getting started page
+// and save it as environment variable into the .env file)
+const token = process.env.WHATSAPP_TOKEN;
+
+// Imports dependencies and set up http server
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  axios = require("axios").default,
+  app = express().use(body_parser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+// Accepts POST requests at /webhook endpoint
 app.post("/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
-  // Check if this is the first incoming message
-  if (/* condición para verificar si es el primer mensaje */) {
-    // Preparar la plantilla de respuesta
-    let messageData = {
-      "messaging_product": "whatsapp",
-      "to": /* número del remitente del mensaje entrante */,
-      "type": "template",
-      "template": {
+  // Check the Incoming webhook message
+  console.log(JSON.stringify(req.body, null, 2));
+
+  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      axios({
+        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data: {
+    "messaging_product": "whatsapp",
+    "to": "51999443948",
+    "type": "template",
+    "template": {
         "name": "warbush_support_2023",
         "language": {
-          "code": "en_US"
+            "code": "en_US"
         },
         "components": [
-          {
-            "type": "button",
-            "sub_type": "flow",
-            "index": "0",
-            "parameters": [
-              {
-                "type": "action",
-                "action": {
-                  "flow_token": "EAAKTv1O8JSoBO8rk8g0uWf3E5xX9pIiRjucZCWJfdB89ynVgQOkrBoN72zoT0BnncfzQfuk64r0Lk1nSwnwErikDcZBCFCtvMMVQYvU8F97qWbypqDNBsVU2OQEKYZA7NZCETowkm6T5scTFzKPl1PdHcxVzXiXr7JE14D0moZAFaGQ4QLZBrTc5eU1LlHZCtd9",
-                  "flow_action_data": {
-                    "<CUSTOM_KEY>": "1445457552683984"
-                  }
-                }
-              }
-            ]
-          }
+            {
+                "type": "button",
+                "sub_type": "flow",
+                "index": "0",
+                "parameters": [
+                    {
+                        "type": "action",
+                        "action": {
+                            "flow_token": "EAAKTv1O8JSoBO8rk8g0uWf3E5xX9pIiRjucZCWJfdB89ynVgQOkrBoN72zoT0BnncfzQfuk64r0Lk1nSwnwErikDcZBCFCtvMMVQYvU8F97qWbypqDNBsVU2OQEKYZA7NZCETowkm6T5scTFzKPl1PdHcxVzXiXr7JE14D0moZAFaGQ4QLZBrTc5eU1LlHZCtd9",
+                            "flow_action_data": {
+                                "<CUSTOM_KEY>": "1445457552683984"
+                            }
+                        }
+                    }
+                ]
+            }
         ]
-      }
-    };
-
-    // Enviar la respuesta usando Request
-    request({
-      url: 'https://graph.facebook.com/v18.0/147014531836606/messages',
-      method: 'POST',
-      headers: {
-        'Authorization': `EAAKTv1O8JSoBO8rk8g0uWf3E5xX9pIiRjucZCWJfdB89ynVgQOkrBoN72zoT0BnncfzQfuk64r0Lk1nSwnwErikDcZBCFCtvMMVQYvU8F97qWbypqDNBsVU2OQEKYZA7NZCETowkm6T5scTFzKPl1PdHcxVzXiXr7JE14D0moZAFaGQ4QLZBrTc5eU1LlHZCtd9`,
-        'Content-Type': 'application/json'
-      },
-      json: messageData
-    }, (error, response, body) => {
-      if (error) {
-        console.error('Error al enviar mensaje:', error);
-      } else {
-        console.log('Mensaje enviado con éxito', body);
-      }
-    });
+    }
+},
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    res.sendStatus(200);
+  } else {
+    // Return a '404 Not Found' if event is not from a WhatsApp API
+    res.sendStatus(404);
   }
+});
 
-  // Resto del código para manejar otros tipos de mensajes entrantes
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests 
+app.get("/webhook", (req, res) => {
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+  **/
+  const verify_token = process.env.VERIFY_TOKEN;
+
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
 });
