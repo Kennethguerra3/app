bf70d6359702954752948a7beb7c67ae6f1533bf
Glitch (messenger-platform-quick-start) <none>
2023-11-15 18:20:45 +0000

    🚅🖼 Checkpoint ./formProcessor.js:43257516/4940

--- formProcessor.js
+++ formProcessor.js
@@ -1,57 +1,82 @@
-// formProcessor.js
-const mysql = require('mysql');
+"use strict";
 
-// Configuración de la conexión a la base de datos
-const db = mysql.createConnection({
-  host: '51.222.24.163:33061',
-  user: 'root',
-  password: '684712',
-  database: 'Formulario_Flow'
-});
+const express = require("express"),
+  body_parser = require("body-parser"),
+  axios = require("axios").default;
+const formProcessor = require('./formProcessor'); // Asegúrate de que esta ruta sea correcta
 
-db.connect((err) => {
-  if (err) { throw err; }
-  console.log('Conectado a la base de datos');
-});
+const app = express().use(body_parser.json());
+const token = process.env.WHATSAPP_TOKEN;
+let lastMessageTime = 0; // Variable para almacenar la hora del último mensaje enviado
 
-function processFormData(body) {
-  if (body.object === "whatsapp_business_account" && body.entry) {
-    body.entry.forEach(entry => {
-      entry.changes.forEach(change => {
-        if (change.field === "messages" && change.value.messages) {
-          change.value.messages.forEach(message => {
-            if (message.interactive && message.interactive.type === "nfm_reply") {
-              let responseJson = message.interactive.nfm_reply.response_json;
-              if (responseJson) {
-                try {
-                  let formData = JSON.parse(responseJson);
-                  let firstName = formData.screen_0_firstName_0;
-                  let lastName = formData.screen_0_lastName_1;
-                  let businessName = formData.screen_0_TextInput_3;
-                  let email = formData.screen_0_email_2;
-                  let options = formData.screen_0_CheckboxGroup_4;
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-                  // Insertar estos datos en la base de datos
-                  let query = "INSERT INTO tu_tabla (firstName, lastName, businessName, email, options) VALUES (?, ?, ?, ?, ?)";
-                  db.query(query, [firstName, lastName, businessName, email, JSON.stringify(options)], (err, result) => {
-                    if (err) {
-                      console.error("Error al insertar en la base de datos", err);
-                    } else {
-                      console.log("Datos insertados en la base de datos", result);
+app.post("/webhook", (req, res) => {
+  let body = req.body;
+
+  // Llamar al procesador de formularios para manejar los datos del formulario
+  formProcessor.processFormData(body);
+
+  // Lógica para enviar mensajes de plantilla cada 4 horas
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from;
+
+      let currentTime = new Date().getTime();
+      if (currentTime - lastMessageTime >= 4 * 60 * 60 * 1000) {
+        lastMessageTime = currentTime;
+
+        axios({
+          method: "POST",
+          url: "https://graph.facebook.com/v12.0/" + phone_number_id + "/messages?access_token=" + token,
+          data: {
+            "messaging_product": "whatsapp",
+            "to": from,
+            "type": "template",
+            "template": {
+              "name": "warbush_support_2023",
+              "language": {
+                "code": "en_US"
+              },
+              "components": [
+                {
+                  "type": "button",
+                  "sub_type": "flow",
+                  "index": "0",
+                  "parameters": [
+                    {
+                      "type": "action",
+                      "action": {
+                        "flow_token": "EAAKTv1O8JSoBO8rk8g0uWf3E5xX9pIiRjucZCWJfdB89ynVgQOkrBoN72zoT0BnncfzQfuk64r0Lk1nSwnwErikDcZBCFCtvMMVQYvU8F97qWbypqDNBsVU2OQEKYZA7NZCETowkm6T5scTFzKPl1PdHcxVzXiXr7JE14D0moZAFaGQ4QLZBrTc5eU1LlHZCtd9",
+                        "flow_action_data": {
+                          "<CUSTOM_KEY>": "1445457552683984"
+                        }
+                      }
                     }
-                  });
-                } catch (error) {
-                  console.error("Error al parsear response_json", error);
+                  ]
                 }
-              }
+              ]
             }
-          });
-        }
-      });
-    });
+          },
+          headers: { "Content-Type": "application/json" },
+        }).then(response => {
+          console.log('Mensaje de plantilla enviado con éxito');
+        }).catch(error => {
+          console.error('Error al enviar el mensaje de plantilla:', error);
+        });
+      }
+    }
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
   }
-}
+});
 
-module.exports = {
-  processFormData
-};
+// ... el resto del código para GET /webhook ...
